7379c7efce7abc9880318f4cea23239148dd343f
ZhengC1 <zhengc1@appstate.edu>
2016-01-25 13:21:39 -0500

    final solution!

--- treasure_hunt.py
+++ treasure_hunt.py
@@ -9,14 +9,15 @@ Class - Artificial Intelligence
 class treasure_hunt(object):
     #reads maps that are 11 by 11
     def __init__(self, map_file):
-        self.lines = [[0 for x in  range(10)] for x in range(10)]
-        self.table = [[1 for x in range(10)] for x in range(10)]
+        self.lines = [[0 for x in  range(11)] for x in range(11)]
+        self.table = [[1 for x in range(11)] for x in range(11)]
         with open(map_file, "r") as treasure_map:
             self.lines = treasure_map.read().splitlines()
         print self.lines
 
     #the recursive function call to solve the map given.
     def backtrack(self, x, y, moves):
+
         value = int(self.lines[x][y])
         print value
 
@@ -27,6 +28,7 @@ class treasure_hunt(object):
             print "found a solution %d %d Return it!" %(x, y)
             return True
         else:
+            self.table[x][y] = 0
             if (x - value) > -1 and (x - value) < 11:
                 print "current position %d %d %s" %(x, y, moves)
                 print "try moving up %d spaces" %value
@@ -59,7 +61,7 @@ class treasure_hunt(object):
                     return True
                 else:
                     self.table[x][y] = 0
-        return None 
+        return False
 
 #prints and asks what file you would like
 file_name = raw_input("name of file :")
